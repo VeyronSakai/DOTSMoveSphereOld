69b17cdd6b604582b8646a21c85c2215f69d4fc0
Yuki Sakai <yuukisakai123@gmail.com>
2019-12-05 15:18:46 +0900

    ChangeForceSystemをEntities0.3.0に対応

--- Assets/Scripts/ChangeForceSystem.cs
+++ Assets/Scripts/ChangeForceSystem.cs
@@ -8,19 +8,22 @@ using UnityEngine;
 
 public class ChangeForceSystem : JobComponentSystem
 {
-    [BurstCompile]
+    // Entities 0.1.1
+    /*[BurstCompile]
     private struct ChangeForceJob : IJobForEach<PhysicsVelocity, PhysicsMass, SphereTagComponentData, Force>
     {
         public float3 Force;
                 
-        public void Execute(ref PhysicsVelocity physicsVelocity, 
+        public void Execute(ref PhysicsVelocity physicsVelocity,
             [ReadOnly] ref PhysicsMass physicsMass, 
-            [ReadOnly] ref SphereTagComponentData sphereComponentData, 
-            ref Force force)
+            [ReadOnly] ref SphereTagComponentData sphereComponentData,
+            ref Force force
+            )
         {
             force.value = Force;
         }
     }
+    
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         var job = new ChangeForceJob();
@@ -46,5 +49,41 @@ public class ChangeForceSystem : JobComponentSystem
         }
             
         return job.Schedule(this, inputDeps);
+    }*/
+    
+    // Entities 0.3.0
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var force = math.float3(0, 0, 0);
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            force = math.float3(-10, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            force = math.float3(10, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            force = math.float3(0, 0, 10);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            force = math.float3(0, 0, -10);
+        }
+
+        var jobHandle = Entities
+            .WithAll<SphereTagComponentData, PhysicsMass, PhysicsVelocity>()
+            .WithBurst()
+            .ForEach((ref Force f) =>
+            {
+                f.value = force;
+            }).Schedule(inputDeps);
+
+        return jobHandle;
     }
 }
